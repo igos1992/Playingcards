1ad36a585bfb0089d946dde60265e573b2596b44
igos1992 <igos19921992@,ail.ru>
2023-08-31 19:29:53 +0300

    solved homework

---
+++ index.js
@@ -0,0 +1,44 @@
+import { renderLevelGame } from './src/renderLevelGame.js';
+
+import './src/style.css';
+export function renderGameDifficulty() {
+    const appElement = document.querySelector('.app');
+
+    const difficultyLevels = `
+    <div class="top center">
+    <form id="form-level" class="box-game" method="POST" action="#">
+        <h1 class="box-game__title">Выбери<br>сложность</h1>
+
+        <div class="box-difficulty-level">
+
+            <input class="box-difficulty-level__radio" type="radio" id="radio1" name="radios" value="6">
+            <label class="box-difficulty-level__label" for="radio1">1</label>
+
+            <input class="box-difficulty-level__radio" type="radio" id="radio2" name="radios" value="12">
+            <label class="box-difficulty-level__label" for="radio2">2</label>
+
+            <input class="box-difficulty-level__radio" type="radio" id="radio3" name="radios" value="18">
+            <label class="box-difficulty-level__label" for="radio3">3</label>
+
+        </div>
+        <button class="box-game__button" id="submit-button" type="submit">Старт</button>
+    </form>
+    </div>`;
+
+    appElement.innerHTML = difficultyLevels;
+
+    document.getElementById('form-level').addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        let levels = document.querySelectorAll('.box-difficulty-level__radio');
+
+        for (const level of levels) {
+            if (level.checked) {
+                console.log(level.value);
+
+                renderLevelGame(level, appElement);
+            }
+        }
+    });
+}
+renderGameDifficulty();
---
+++ src/arrayCards.js
@@ -0,0 +1,46 @@
+export const cardsSuitsArr = [
+    `<img class="game-cards__flip-side" src="./static/sixHearts.png">`,
+    `<img class="game-cards__flip-side" src="./static/sevenHearts.png">`,
+    `<img class="game-cards__flip-side" src="./static/eightHearts.png">`,
+    `<img class="game-cards__flip-side" src="./static/nineHearts.png">`,
+
+    `<img class="game-cards__flip-side" src="./static/tenHearts.png">`,
+    `<img class="game-cards__flip-side" src="./static/jackHearts.png">`,
+    `<img class="game-cards__flip-side" src="./static/queenHearts.png">`,
+    `<img class="game-cards__flip-side" src="./static/kingHearts.png">`,
+
+    `<img class="game-cards__flip-side" src="./static/aceOfHearts.png">`,
+    `<img class="game-cards__flip-side" src="./static/sixClubs.png">`,
+    `<img class="game-cards__flip-side" src="./static/sevenClubs.png">`,
+    `<img class="game-cards__flip-side" src="./static/eightClubs.png">`,
+
+    `<img class="game-cards__flip-side" src="./static/nineClubs.png">`,
+    `<img class="game-cards__flip-side" src="./static/tenClubs.png">`,
+    `<img class="game-cards__flip-side" src="./static/jackClubs.png">`,
+    `<img class="game-cards__flip-side" src="./static/queenClubs.png">`,
+
+    `<img class="game-cards__flip-side" src="./static/kingClubs.png">`,
+    `<img class="game-cards__flip-side" src="./static/aceOfClubs.png">`,
+    `<img class="game-cards__flip-side" src="./static/sixDiamonds.png">`,
+    `<img class="game-cards__flip-side" src="./static/sevenDiamonds.png">`,
+
+    `<img class="game-cards__flip-side" src="./static/eightDiamonds.png">`,
+    `<img class="game-cards__flip-side" src="./static/nineDiamond.png">`,
+    `<img class="game-cards__flip-side" src="./static/tenDiamonds.png">`,
+    `<img class="game-cards__flip-side" src="./static/jackDiamond.png">`,
+
+    `<img class="game-cards__flip-side" src="./static/queenDiamonds.png">`,
+    `<img class="game-cards__flip-side" src="./static/kingDiamonds.png">`,
+    `<img class="game-cards__flip-side" src="./static/aceOfDiamonds.png">`,
+    `<img class="game-cards__flip-side" src="./static/sixSpades.png">`,
+
+    `<img class="game-cards__flip-side" src="./static/sevenSpades.png">`,
+    `<img class="game-cards__flip-side" src="./static/eightSpades.png">`,
+    `<img class="game-cards__flip-side" src="./static/nineSpades.png">`,
+    `<img class="game-cards__flip-side" src="./static/tenSpades.png">`,
+
+    `<img class="game-cards__flip-side" src="./static/jackSpades.png">`,
+    `<img class="game-cards__flip-side" src="./static/queenSpades.png">`,
+    `<img class="game-cards__flip-side" src="./static/kingSpades.png">`,
+    `<img class="game-cards__flip-side" src="./static/aceOfSpades.png">`,
+];
